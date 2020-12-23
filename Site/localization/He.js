907e85d6052a5c77478f1a042ec2704cea6d73cb
yossistarz <yossistarz@gmail.com>
2020-12-23 19:31:15 +0200

    fixed popup not woriking

--- Site/localization/He.js
+++ Site/localization/He.js
@@ -2,5 +2,7 @@
     "loading" : "בטעינה",
     "Main View" : "מסך ראשי",
     "Show Popup":"הצג מסך קופץ",
-    "Show Loading":"הצג טעינה"
+    "Show Loading":"הצג טעינה",
+    "Popup Title":"כותרת פופאפ",
+    "Close Msg":"סגור"
 };
